0e1f43107793f2eef6f47fd6b1c593da54e3ea47
Kent Dodds <kent@doddsfamily.us>
2013-06-12 02:48:51 -0500

    My file hack. Helpful for now… Hopefully I'll be able to figure out how to do this for real later...

---
+++ common/ResetDatabase.js
@@ -0,0 +1,48 @@
+var _ = require('underscore');
+var dropCollections = false;
+
+var ResetDatabase = (function() {
+  var mongoose = require('mongoose');
+  
+  var db = require('../controllers/db');
+  var i = 0;
+  db.connectMongoose();
+  console.log('Connected...');
+  if (dropCollections) {
+    db.setupResources();
+    console.log('Dropping Collections');
+    for (i = 0; i < db.resources.length; i++) {
+      var resource = db.resources[i];
+      var callback = function(err) {
+        var collectionName = resource.uriName;
+        if (err) {
+          console.warn('Error dropping ' + collectionName);
+          console.warn(err);
+        } else {
+          console.log('Dropped ' + collectionName + ' successfaully');
+        }
+      }
+      mongoose.connection.collections[resource.uriName].drop(callback);
+    }
+  } else {
+    console.log('Loading mocks...');
+    var models = require('../test/integration/Mocks').models;
+    var keys = _.keys(models);
+    console.log('Saving mocks...');
+    for (i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      var modelMocks = models[key];
+      for (var j = 0; j < modelMocks.length; j++) {
+        var model = modelMocks[j];
+        if (!(key === 'Votes' && _.isUndefined(model.points))) {
+          model.save();
+        }
+      }
+    }
+    console.log('Mocks saved!');
+  }
+  return {
+  }
+})();
+
+module.exports = ResetDatabase;
